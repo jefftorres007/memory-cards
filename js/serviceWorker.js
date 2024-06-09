4f272e9b4820a9c09610ffa56a7d0ded63d2186e
JEFERSON_LENOVO\jeff <torresjefferson007@hotmail.com>
2024-06-09 23:03:09 +0200

    estable 2

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -7,9 +7,19 @@ const assets = [
   "/js/gameData.js",
   "/js/router.js",
   "/js/routers.js",
-  "/favicon.png",
+  "/favicon.ico",
   "/sounds/lose.mp3",
   "/sounds/win.mp3",
+  
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png",
+
 
 ];
 
